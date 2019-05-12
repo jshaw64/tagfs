387c6940c6df53557273cc1800cccd451bf4b5cb
jshaw <jshaw64@gmail.com>
2019-05-12 12:50:06 -0400

    Add basic support for creating new entries to cli interface

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 import json
+import sys
+
+name = sys.argv[1]
+description = sys.argv[2]
+value = sys.argv[3]
+tags = sys.argv[4]
 
 with open('sample.json') as json_file:
     data = json.load(json_file)
 
 with open('sample.json', 'w') as json_file:
     data["entries"].append({
-            "hello": "world"
+            "name": name,
+            "description": description,
+            "value": value,
+            "tags": [ tags ]
         }
     )
     json_file.write(json.dumps(data, indent=4))
