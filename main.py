f230dc1b40b64dc0b087fb5418beb536c020fbd2
jshaw <jshaw64@gmail.com>
2019-05-12 13:07:11 -0400

    Wrap write step in function
    So we can easily switch on cli args

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ description = sys.argv[2]
 value = sys.argv[3]
 tags = sys.argv[4].split(" ")
 
-with open('sample.json') as json_file:
-    data = json.load(json_file)
+def runWrite():
+    with open('sample.json') as json_file:
+        data = json.load(json_file)
 
-with open('sample.json', 'w') as json_file:
-    data["entries"].append({
-            "name": name,
-            "description": description,
-            "value": value,
-            "tags": tags
-        }
-    )
-    json_file.write(json.dumps(data, indent=4))
+    with open('sample.json', 'w') as json_file:
+        data["entries"].append({
+                "name": name,
+                "description": description,
+                "value": value,
+                "tags": tags
+            }
+        )
+        json_file.write(json.dumps(data, indent=4))
+
+runWrite()
