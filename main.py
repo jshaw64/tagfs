391ed1c5089840844243029872f866ab1381c820
jshaw <jshaw64@gmail.com>
2019-05-12 20:01:21 -0400

    Remove inline print from get_latest_dataset
    Now that we have `print_latest_dataset`

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ def write_json(output):
 def get_latest_dataset():
     list_of_files = glob.glob('./data/*')
     latest_file = max(list_of_files, key=os.path.getctime)
-    print(latest_file)
+
+    return latest_file
 
 def print_latest_dataset():
     with open(get_latest_dataset()) as json_file:
